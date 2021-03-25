8f0dc7107d755adffc9f14c6cd22e4219c9a4c11
marcs <m@marcsist.com>
2021-03-25 23:05:14 +0100

    added buefy and dependencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 const { createApp } = require('vue');
 import App from "./App.vue";
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
 
 createApp(App).mount("#app");
